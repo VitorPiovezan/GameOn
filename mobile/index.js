100c4aa63fa97ab9c801a4c6177063b6a3eff11b
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2020-02-19 21:18:00 -0300

    Erro index corrigido

--- mobile/index.js
+++ mobile/index.js
@@ -2,3 +2,5 @@ import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 
 import Routes from "./src/components/Login/routes";
+
+AppRegistry.registerComponent(appName, () => Routes);
